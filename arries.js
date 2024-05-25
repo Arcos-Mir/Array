c492102906cbd1f0d2051dda525fc60544e25c80
Mirza Arcos <166764651+Arcos-Mir@users.noreply.github.com>
2024-05-24 23:35:01 -0600

    Add files via upload

---
+++ arries.js
@@ -0,0 +1,26 @@
+/* se les conoce tambien como arrais, vectores o matrices
+es una variable que puede guardar multiples valores*/
+
+/*var nombre = 'Mir';
+var segundoNombre = 'Itzel';
+var apellido = 'aRCOS'; ES un ejemplo de las multiples variables que se pueden agregar 
+en un documento*/
+
+//var amigos = ['Carlos', 'Alejandro', 'Cesar', 123, true]; ejemplo de arreglo
+
+var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 
+        'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 
+        'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+/*para acceder a algun mes  se coloca la siguiente linea de copdigo en la cual se especifica un numero
+iniciando desde "0 hasta el valor del mes de diciembre el cual seria 11, los arreglos se enumeran desde 0
+seria el mes de enero y si se le coloca el valor 12 para acceder se muestra en pantalla la palabra undefined
+para decir que ese valor no existe "*/
+//document.write(meses[5]);
+
+console.log(meses);
+
+// ----- 
+
+var amigos = [];
+amigos[0] = 'Carlos';
+amigos[1] = 'Alejandro';
